3ab3b70fccf4392f5959457f0835af89f0603945
Akmal0906 <akmaljoraqulov1@gmail.com>
2025-01-22 10:02:27 +0500

    finishied

--- lib/features/search/presentation/widgets/search_menu_widget.dart
+++ lib/features/search/presentation/widgets/search_menu_widget.dart
@@ -24,71 +24,73 @@ class SearchMenuWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 32),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 56,
-            child: TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                fillColor: AppColors.color18,
-                filled: true,
-                enabledBorder: InputBorder.none,
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(color: Colors.black),
-                ),
-                errorBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                hintText: 'Search',
-                suffixIcon: IconButton(
-                    onPressed: () {
-                      onTap();
-                      if(isTapped){
-
-                      }
-                    },
-                    icon:isTapped?Icon(Icons.clear): Icon(Icons.search)),
-              ),
-            ),
-          ),
-          ListView.separated(
-            itemCount: menu.length,
-            physics: NeverScrollableScrollPhysics(),
-            padding: EdgeInsets.only(left: 8, right: 8, top: 32),
-            shrinkWrap: true,
-            separatorBuilder: (BuildContext context, int index) =>
-                index < menu.length - 1
-                    ? Divider(
-                        thickness: 1,
-                        color: Colors.grey,
-                      )
-                    : SizedBox.shrink(),
-            itemBuilder: (BuildContext context, int index) => GestureDetector(
-              onTap: () {
-              onTap1();
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      menu[index],
-                      style: TextStyle(
-                          fontFamily: "Inter",
-                          fontWeight: FontWeight.w600,
-                          fontSize: 20,
-                          color: AppColors.color17),
-                    ),
-                    SvgPicture.asset(SvgIcons.icForward)
-                  ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 56,
+              child: TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  fillColor: AppColors.color18,
+                  filled: true,
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  errorBorder: InputBorder.none,
+                  disabledBorder: InputBorder.none,
+                  hintText: 'Search',
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        onTap();
+                        if(isTapped){
+        
+                        }
+                      },
+                      icon:isTapped?Icon(Icons.clear): Icon(Icons.search)),
                 ),
               ),
             ),
-          ),
-        ],
+            ListView.separated(
+              itemCount: menu.length,
+              physics: NeverScrollableScrollPhysics(),
+              padding: EdgeInsets.only(left: 8, right: 8, top: 32),
+              shrinkWrap: true,
+              separatorBuilder: (BuildContext context, int index) =>
+                  index < menu.length - 1
+                      ? Divider(
+                          thickness: 1,
+                          color: Colors.grey,
+                        )
+                      : SizedBox.shrink(),
+              itemBuilder: (BuildContext context, int index) => GestureDetector(
+                onTap: () {
+                onTap1();
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        menu[index],
+                        style: TextStyle(
+                            fontFamily: "Inter",
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20,
+                            color: AppColors.color17),
+                      ),
+                      SvgPicture.asset(SvgIcons.icForward)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
